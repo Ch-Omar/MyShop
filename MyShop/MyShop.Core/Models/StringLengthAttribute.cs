2f4ec13b0d87060d49ba08e5c0ebfdea791c7d99
Omar Chaktoun <ch-omar@live.fr>
2020-03-10 22:44:53 +0100

    Product and InMemory Product Repository Build

---
+++ MyShop/MyShop.Core/Models/StringLengthAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyShop.Core.Models
+{
+    internal class StringLengthAttribute : Attribute
+    {
+        private int v;
+
+        public StringLengthAttribute(int v)
+        {
+            this.v = v;
+        }
+    }
+}
