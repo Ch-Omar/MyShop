4ebedadfa13955d3c6d84392c4fdcefd5a08e563
Omar Chaktoun <ch-omar@live.fr>
2020-03-16 19:16:39 +0100

    Implemented generic Repository

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -11,13 +11,13 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
 
         public ProductManagerController()
         {
-            context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -44,7 +44,7 @@ namespace MyShop.WebUI.Controllers
             else
             {
                 context.Insert(product);
-                context.commit();
+                context.Commit();
 
                 return RedirectToAction("Index");
             }
@@ -88,7 +88,7 @@ namespace MyShop.WebUI.Controllers
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
 
-                context.commit();
+                context.Commit();
 
                 return RedirectToAction("Index");
             }
@@ -119,7 +119,7 @@ namespace MyShop.WebUI.Controllers
             else
             {
                 context.Delete(Id);
-                context.commit();
+                context.Commit();
                 return RedirectToAction("Index");
             }
         }
